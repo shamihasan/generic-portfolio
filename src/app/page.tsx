3a7c17d37904942c1223b32f37d64a7b82b6464e
shubh-mehrotra <shubh0896m@gmail.com>
2026-05-28 12:44:06 +0530

    feat: Add Projects section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Projects from "@/components/Projects"; // Import the new Projects component
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Projects /> {/* Add the Projects component here */}
     </main>
   );
 }
